b4b3125d78e0b2b8d5c2a703bc7d108a2c83aa99
root <root@LOC7455.BBS2.local>
2022-03-19 01:25:49 -0300

    Update project IDs and buckets

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "bs2-tfstate-noprd"
     prefix = "env/dev"
   }
 }
--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="bs2-no-prd-host-project"
